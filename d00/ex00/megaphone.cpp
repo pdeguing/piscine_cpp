d2916e9cf1504e112eca07c00b678d19ee8fa667
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-04-06 10:48:49 -0700

    Cleaned a bit

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -1,9 +1,17 @@
+/**
+ * File              : megaphone.cpp
+ * Author            : pdeguing <pdeguing@student.42.us.org>
+ * Date              : 06.04.2019
+ * Last Modified Date: 06.04.2019
+ * Last Modified By  : pdeguing <pdeguing@student.42.us.org>
+ */
 #include <iostream>
 #include <cctype>
 
 int	main(int ac, char **av) {
 	if (ac < 2) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
 	}
 	for (int i = 1; i < ac; i++) {
 		for (int j = 0; av[i][j]; j++) {
