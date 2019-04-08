c5cd6b55aca986cd9488b01e8721b4e0c90b7c20
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-04-08 10:26:13 -0700

    Created separate functions

--- d01/ex00/main.cpp
+++ d01/ex00/main.cpp
@@ -8,18 +8,28 @@
 
 #include "Pony.hpp"
 
-int	main(void) {
-	Pony	Booby = Pony("Booby", "White", false, false);
+void	ponyOnTheHeap(void) {
 	Pony	*Jimmy = new Pony("Jimmy", "Blue,Red,Yellow", true, true);
-
-	std::cout << "Booby is on the stack, but Jimmy is on the heap, how cool is that??" << std::endl;
-	std::cout << "This is little Booby:" << std::endl;
-	Booby.draw();
 	std::cout << "This is little Jimmy:" << std::endl;
 	Jimmy->draw();
 	std::cout << "Now Jimmy must go away to a magical country... He has to fulfill his destiny..." << std::endl;
 	delete Jimmy;
 	std::cout << "He is gone. O_o" << std::endl << std::endl;
+	return ;
+}
+
+void	ponyOnTheStack(void) {
+	Pony	Booby = Pony("Booby", "White", false, false);
+	std::cout << "This is little Booby:" << std::endl;
+	Booby.draw();
 	std::cout << "Time to return home Booby!" << std::endl;
+	return ;
+}
+
+int	main(void) {
+
+	std::cout << "Booby is on the stack, but Jimmy is on the heap, how cool is that??" << std::endl;
+	ponyOnTheHeap();
+	ponyOnTheStack();
 	return 0;
 }
